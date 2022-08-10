32937b209a4229744fbd22753cf4882b75a67dfc
Usman <shahjaayy@gmail.com>
2022-08-11 00:28:22 +0500

    Initial commit

---
+++ app/src/main/java/com/fascinate/flowsdemo/CounterActivity.kt
@@ -0,0 +1,11 @@
+package com.fascinate.flowsdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CounterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_counter)
+    }
+}
